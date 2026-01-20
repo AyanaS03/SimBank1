df4fcfee8cdc1ee2eadb26832acfa02e8e3737a3
Ayana <satylganovaaiana@gmail.com>
2026-01-20 18:19:15 +0600

    my name

--- src/revertAndReset/RevertDemo.java
+++ src/revertAndReset/RevertDemo.java
@@ -3,5 +3,6 @@ package revertAndReset;
 public class RevertDemo {
     public static void main(String[] args) {
         System.out.println("demo 1");
+        System.out.println(My name is Ayana);
     }
 }
