0451068490fa3f7a481f062f8cb4a034fedc0c20
Ayana <satylganovaaiana@gmail.com>
2026-01-20 18:32:40 +0600

    Revert "my name"
    This reverts commit df4fcfee8cdc1ee2eadb26832acfa02e8e3737a3.

--- src/revertAndReset/RevertDemo.java
+++ src/revertAndReset/RevertDemo.java
@@ -3,6 +3,5 @@ package revertAndReset;
 public class RevertDemo {
     public static void main(String[] args) {
         System.out.println("demo 1");
-        System.out.println(My name is Ayana);
     }
 }
